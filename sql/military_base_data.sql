6360d106ec0e0171a01f9a1a4873e70f529fd82f
Александр Коваленко <merlinwt@gmail.com>
2015-12-26 19:36:10 +0300

    and more tanks!!!

--- sql/military_base_data.sql
+++ sql/military_base_data.sql
@@ -3,6 +3,7 @@ INSERT INTO military_base.tank (tank_name, tank_armor_lob, tank_armor_side, tank
 INSERT INTO military_base.tank (tank_name, tank_armor_lob, tank_armor_side, tank_armor_rear)VALUES ('Тигр', 100, 80, 80);
 INSERT INTO military_base.tank (tank_name, tank_armor_lob, tank_armor_side, tank_armor_rear)VALUES ('ИС', 120, 90, 60);
 INSERT INTO military_base.tank (tank_name, tank_armor_lob, tank_armor_side, tank_armor_rear)VALUES ('КВ-1', 75, 75, 70);
+INSERT INTO military_base.tank (tank_name, tank_armor_lob, tank_armor_side, tank_armor_rear)VALUES ('МС-1', 18, 16, 16);
 /*Орудия*/
 INSERT INTO military_base.gun(gun_name, gun_caliber ,gun_penetratoin)VALUES ('Л-11', 68, 76);
 INSERT INTO military_base.gun(gun_name, gun_caliber ,gun_penetratoin)VALUES ('Ф-34', 86, 76);
@@ -19,6 +20,11 @@ INSERT INTO military_base.gun(gun_name, gun_caliber ,gun_penetratoin)VALUES ('Д
 INSERT INTO military_base.gun(gun_name, gun_caliber ,gun_penetratoin)VALUES ('ЗиС-5', 76, 86);
 INSERT INTO military_base.gun(gun_name, gun_caliber ,gun_penetratoin)VALUES ('У-11', 122, 61);
 INSERT INTO military_base.gun(gun_name, gun_caliber ,gun_penetratoin)VALUES ('проект 413', 57, 112);
+
+INSERT INTO military_base.gun(gun_name, gun_caliber ,gun_penetratoin)VALUES ('Б-3', 37, 40);
+INSERT INTO military_base.gun(gun_name, gun_caliber ,gun_penetratoin)VALUES ('ВЯ', 23, 30);
+INSERT INTO military_base.gun(gun_name, gun_caliber ,gun_penetratoin)VALUES ('Гочкис', 37, 34);
+INSERT INTO military_base.gun(gun_name, gun_caliber ,gun_penetratoin)VALUES ('ТНШ', 20, 28);
 /*Совсестивость*/
 /*Т34*/
 INSERT INTO military_base.tank_gun(tank_id, gun_id) VALUES (1,1);
@@ -32,3 +38,14 @@ INSERT INTO military_base.tank_gun(tank_id, gun_id) VALUES (2,6);
 INSERT INTO military_base.tank_gun(tank_id, gun_id) VALUES (3,7);
 INSERT INTO military_base.tank_gun(tank_id, gun_id) VALUES (3,8);
 INSERT INTO military_base.tank_gun(tank_id, gun_id) VALUES (3,9);
+/*КВ-1*/
+INSERT INTO military_base.tank_gun(tank_id, gun_id) VALUES (4,10);
+INSERT INTO military_base.tank_gun(tank_id, gun_id) VALUES (4,11);
+INSERT INTO military_base.tank_gun(tank_id, gun_id) VALUES (4,12);
+/*МС-1*/
+INSERT INTO military_base.tank_gun(tank_id, gun_id) VALUES (5,13);
+INSERT INTO military_base.tank_gun(tank_id, gun_id) VALUES (5,14);
+INSERT INTO military_base.tank_gun(tank_id, gun_id) VALUES (5,15);
+
+
+
